bbf55d7a40436d40c6df0ae085e98ce6acce3b6b
Andrei Tobias <tobias_andrei@yahoo.com>
2016-11-21 16:56:46 +0000

    Added mailer and view

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,11 @@
+class ContactMailer < ActiveMailer::Base
+    default to: tobias_andrei@yahoo.com
+    
+    def contact_email (name, email, body)
+        @name = name
+        @email = email
+        @body = body
+        
+    mail(from: email, subject: 'Contact Form Message')    
+    end    
+end    
