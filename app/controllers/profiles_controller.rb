755e996c0411d36d7db344bbaec1fd406df4ee3a
Andrei Tobias <tobias_andrei@yahoo.com>
2016-12-12 20:57:16 +0000

    Added barebones user show page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,19 +5,19 @@ class ProfilesController < ApplicationController
         @profile = @user.build_profile
     end
     
-    def create
-        @user = User.find ( params[:user_id] ) 
-        @profile = @user.build_profile
-        if @profile.save
-            flash[:success] = "Profile Updated!"
-            redirect_to user_path( params[:user_id] )
-        else
-            render action: :new
-        end    
+    def create 
+    @user = User.find( params[:user_id] )
+    @profile = @user.build_profile(profile_params)
+    if @profile.save
+      flash[:success] = "Profile Updated!"
+      redirect_to user_path( params[:user_id] )
+    else
+      render action: :new
     end
-    
-    private 
+  end
+  
+  private
     def profile_params
-        params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description) 
-    end    
-end    
+      params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+    end
+end
