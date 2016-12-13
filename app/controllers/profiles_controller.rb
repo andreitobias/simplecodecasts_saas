93e6f32f8a13d00a2a0c1e9d0c0a2465a9567196
Andrei Tobias <tobias_andrei@yahoo.com>
2016-12-13 16:26:34 +0000

    Added from partial and added action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,15 +6,20 @@ class ProfilesController < ApplicationController
     end
     
     def create 
-    @user = User.find( params[:user_id] )
-    @profile = @user.build_profile(profile_params)
-    if @profile.save
-      flash[:success] = "Profile Updated!"
-      redirect_to user_path( params[:user_id] )
-    else
-      render action: :new
+      @user = User.find( params[:user_id] )
+      @profile = @user.build_profile(profile_params)
+      if @profile.save
+        flash[:success] = "Profile Updated!"
+        redirect_to user_path( params[:user_id] )
+      else
+        render action: :new
+      end
     end
-    end
+    
+    def edit 
+      @user = User.find( params[:user_id] )
+      @profile = @user.profile
+    end   
   
   private
     def profile_params
